c984b6ab9f73dae03af72452784c2c2f752b14fa
Rajendra Badgujar <rajendra.badgujar.sid@gmail.com>
2016-08-04 19:29:18 +0530

    work in comment api

---
+++ src/accounts/api/serializers.py
@@ -0,0 +1,112 @@
+from rest_framework.serializers import (
+	ModelSerializer, 
+	HyperlinkedIdentityField, 
+	SerializerMethodField,
+	ValidationError,
+	EmailField,
+	CharField
+	)
+from django.db.models import Q
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class UserCreateSerializer(ModelSerializer):
+	email = EmailField(label='Email Address')
+	email2 = EmailField(label='Confirm Email')
+	class Meta:
+		model = User
+		fields = [
+			'username',
+			'email',
+			'email2',
+			'password',
+		]
+		extra_kwargs = {"password":
+							{"write_only": True}
+							}
+
+	def validate(self, data):
+		# email = data['email']
+		# user_qs = User.objects.filter(email=email)
+		# if user_qs.exists():
+		# 	raise ValidationError("This user already registered.")
+		return data
+
+	def validated_email(self, value):
+		data = self.get_initial()
+		email1 = data.get("email2")
+		email2 = value
+		if email1 != email2:
+			raise ValidationError("Email must match")
+
+		user_qs = User.objects.filter(email=email2)
+		if user_qs.exists():
+			raise ValidationError("This user already registered.")
+
+		return value
+
+	def validated_email2(self, value):
+		data = self.get_initial()
+		email1 = data.get("email")
+		email2 = value
+		if email1 != email2:
+			raise ValidationError("Email must match")
+		return value
+
+	def create(self, validated_data):
+		username = validated_data['username']
+		email = validated_data['email']
+		password = validated_data['password']
+		user_obj = User(
+				username = username,
+				email = email,
+			)
+		user_obj.set_password(password)
+		user_obj.save()
+		return validated_data
+
+
+class UserLoginSerializer(ModelSerializer):
+	token = CharField(allow_blank=True, read_only=True)
+	username = CharField(required=False, allow_blank=True)
+	email = EmailField(label='Email Address', required=False, allow_blank=True)
+	class Meta:
+		model = User
+		fields = [
+			'username',
+			'email',
+			'password',
+			'token',
+		]
+		extra_kwargs = {"password":
+							{"write_only": True}
+							}
+
+	def validate(self, data):
+		user_obj = None
+		email = data.get("email", None)
+		username = data.get("username", None)
+		password = data["password"]
+		if not email and not username:
+			raise ValidationError("A username or email is required to login.")
+
+		user = User.objects.filter(
+				Q(email=email) |
+				Q(username=username)
+			).distinct()
+		# user = user.exclude(email_isnull=True).exclude(email_iexact='')
+		if user.exists() and user.count() ==1:
+			user_obj = user.first()
+		else:
+			raise ValidationError("This username/email is not valid.")
+
+		if user_obj:
+			if not user_obj.check_password(password):
+				raise ValidationError("Incorrect credentials please try again.")
+
+		data["token"] = "SOME RANDOM TOKEN"
+		return data
---
+++ src/posts/api/views.py
@@ -0,0 +1,94 @@
+from django.db.models import Q
+
+from rest_framework.filters import (
+	SearchFilter,
+	OrderingFilter,
+	)
+
+from rest_framework.pagination import (
+	LimitOffsetPagination,
+	PageNumberPagination,
+	)
+
+from rest_framework.generics import (
+	CreateAPIView,
+	DestroyAPIView,
+	ListAPIView, 
+	UpdateAPIView,
+	RetrieveAPIView,
+	RetrieveUpdateAPIView
+	)
+
+from posts.models import Post
+from .serializers import (
+	PostDetailSerializer,
+	PostListSerializer,
+	PostCreateUpdateSerializer
+	)
+
+from .pagination import PostLimitOffsetPagiantion, PostPageNumberPagination
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
+	)
+
+
+class PostCreateAPIView(CreateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostCreateUpdateSerializer
+	permission_classes = [IsAuthenticated]
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+
+
+class PostDetailAPIView(RetrieveUpdateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+	# lookup_url_kwargs = "abc"
+
+
+class PostUpdateAPIView(RetrieveUpdateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostCreateUpdateSerializer
+	lookup_field = 'slug'
+	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+	# lookup_url_kwargs = "abc"
+
+	def perform_update(self, serializer):
+		serializer.save(user=self.request.user)
+
+
+class PostDeleteAPIView(DestroyAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+	# lookup_url_kwargs = "abc"
+
+
+class PostListAPIView(ListAPIView):
+	serializer_class = PostListSerializer
+	filter_backends = [SearchFilter]
+	search_fields = ['title', 'content', 'user_first_name']
+	pagination_class = PostPageNumberPagination #PageNumberPagination
+
+	def get_queryset(self, *args, **kwargs):
+		# queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+		queryset_list = Post.objects.all()
+		query = self.request.GET.get("q")
+		if query:
+			queryset_list = queryset_list.filter(
+					Q(title__icontains=query)|
+					Q(content__icontains=query)|
+					Q(user__first_name__icontains=query) |
+					Q(user__last_name__icontains=query)
+					).distinct()
+		return queryset_list
+
+
+
